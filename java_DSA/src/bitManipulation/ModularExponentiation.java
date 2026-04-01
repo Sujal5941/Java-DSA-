bcd1a640009f223d1f4cb7e93f945410e2bd9c88
Sujal <sujalshirude592005@gmail.com>
2026-04-01 22:42:53 +0530

    Bit Manipulation

--- java_DSA/src/bitManipulation/ModularExponentiation.java
+++ java_DSA/src/bitManipulation/ModularExponentiation.java
@@ -1,17 +1,18 @@
 package bitManipulation;
 import java.util.*;
-public class FastExponentiation {
+public class ModularExponentiation {
 
-	public static int FastExponent(int a,int n) {
+	public static int ModularExponent(int a,int n,int m) {
 		int ans = 1;
+		a = a % m;
 		while(n>0) {
-			if((n & 1) != 0) {
-				ans = ans * a;
+			if((n&1) != 0) {
+				ans = (ans * a)%m;
 			}
-			a = a * a;
-			n = n >> 1;
+			a = (a*a)%m;
+			n=n>>1;
 		}
-		return ans; 
+		return ans;
 	}
 	
 	public static void main(String[] args) {
@@ -21,8 +22,10 @@ public class FastExponentiation {
 		int a = s.nextInt();
 		System.out.println("Enter the Exponent : ");
 		int n = s.nextInt();
+		System.out.println("Enter the Modulus value : ");
+		int m = s.nextInt();
 		
-		System.out.println("Fast Exponentiation = "+FastExponent(a,n));
+		System.out.println("Modulus of "+a+" to the power "+n+" is : "+ModularExponent(a,n,m));
 	}
 
 }
